821e5261297014f7de335cd21b2aa055f4787ef1
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-11-25 21:37:58 +0500

    Add shared authentication, error, config, and validator modules
    Introduces middleware for role-based access and login checks, centralized error handling and custom error classes, configuration management via environment variables, and a Joi-based request validator. These utilities provide foundational support for authentication, error management, configuration, and validation in the application.

---
+++ src/shared/config/index.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const config = {
+  port: process.env.PORT,
+  db: {
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expirec_in: process.env.JWT_EXPIRES_IN,
+  },
+  telegram: {
+    token: process.env.BOT_TOKEN,
+  },
+};
+
+module.exports = config;
